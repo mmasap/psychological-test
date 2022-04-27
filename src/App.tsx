07ea821a6cdf7a117ec641a1c83c83a042dff844
mmasap <miura.worker@gmail.com>
2022-04-27 23:42:17 +0900

    fix github pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import AdminPage from './pages/AdminPage';
 import IndexPage from './pages/IndexPage';
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<IndexPage />} />
-        <Route path="admin" element={<AdminPage />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<IndexPage />} />
+      <Route path="admin" element={<AdminPage />} />
+    </Routes>
   );
 };
 
