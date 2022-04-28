91d16755b31619080cefd3d7a3e6bf1d7f696ad9
mmasap <miura.worker@gmail.com>
2022-04-28 23:10:37 +0900

    fix path

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ const App = () => {
   return (
     <Routes>
       <Route path="/" element={<IndexPage />} />
-      <Route path="admin" element={<AdminPage />} />
+      <Route path="/admin" element={<AdminPage />} />
     </Routes>
   );
 };
