ea4a7624e2a7ee77434b3340fef3784c3723ed8f
mmasap <miura.worker@gmail.com>
2022-06-02 17:29:44 +0900

    change url from env file

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,9 +1,3 @@
 import axios from 'axios';
 
-let baseURL = '';
-
-if (process.env.NODE_ENV === 'production') {
-  baseURL = 'https://psychological-test-934b7-default-rtdb.firebaseio.com';
-}
-
-export default axios.create({ baseURL });
+export default axios.create({ baseURL: process.env.REACT_APP_API_ENDPOINT });
