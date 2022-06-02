8df7303524c99c4b77c05c01eca82a6bb852ecae
mmasap <miura.worker@gmail.com>
2022-06-02 17:29:19 +0900

    fix mock service worker

--- src/index.tsx
+++ src/index.tsx
@@ -3,25 +3,14 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
-async function main() {
-  if (process.env.NODE_ENV === 'development') {
-    if (window.location.pathname === '/psychological-test') {
-      window.location.pathname = '/psychological-test/';
-      return;
-    }
-    const { worker } = require('./mocks/browser');
-    await worker.start({
-      serviceWorker: {
-        url: '/psychological-test/mockServiceWorker.js',
-      },
-    });
-  }
-
-  ReactDOM.render(
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <App />
-    </BrowserRouter>,
-    document.getElementById('root')
-  );
+if (process.env.NODE_ENV === 'development') {
+  const { worker } = require('./mocks/browser');
+  worker.start();
 }
-main();
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root')
+);
